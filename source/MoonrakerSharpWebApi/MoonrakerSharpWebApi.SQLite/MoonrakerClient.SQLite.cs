9af0d339ff6c041b27529a8e675ebe9a0e801f64
Andreas <andy.reitberger@gmail.com>
2023-02-01 07:17:15 +0100

    Further improvements

--- source/MoonrakerSharpWebApi/MoonrakerSharpWebApi.SQLite/MoonrakerClient.SQLite.cs
+++ source/MoonrakerSharpWebApi/MoonrakerSharpWebApi.SQLite/MoonrakerClient.SQLite.cs
@@ -3,6 +3,9 @@ using SQLite;
 
 namespace AndreasReitberger.API.Moonraker.Extensions
 {
+    /// <summary>
+    /// This is a prepartion to make the client workable with SQlite, however not yet ready.
+    /// </summary>
     [Table(nameof(MoonrakerClient) + "s")]
     public partial class MoonrakerSqliteClient : MoonrakerClient
     {
